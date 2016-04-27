371fb8b2c0d8efdf80af9b3a2742a7db990ff8aa
Edward Dubovic <edward.dubovic@gmail.com>
2016-04-27 23:54:12 +0300

    Roll a Ball

---
+++ Assets/RollaBall/Scripts/CirclePickUp.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CirclePickUp : MonoBehaviour {
+
+    public Object PickUp;
+    private float distance = 8;
+    private int count = 12;
+
+    void Start () {
+        DrawCircle(transform.position);
+    }
+
+    private void DrawCircle(Vector3 point) {
+        point.y = 1;
+        float angle = 360 * Mathf.Deg2Rad;
+        for (int i = 1; i <= count; i++) {
+            point.z = transform.position.z + Mathf.Cos(angle / count * i) * distance;
+            point.x = transform.position.x + Mathf.Sin(angle / count * i) * distance;
+            GameObject.Instantiate(PickUp, point, Quaternion.identity);
+        }
+    }
+}
